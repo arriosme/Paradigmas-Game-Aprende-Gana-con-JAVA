8dd826479bbdd52f996183363c9e7135fadb1bb4
adrycn8 <acortesn@poligran.edu.co>
2022-06-13 19:41:27 -0500

    Documentación final 7:41 pm

--- src/com/gameapp/controller/LoginSceneController.java
+++ src/com/gameapp/controller/LoginSceneController.java
@@ -1,14 +1,22 @@
-/*Creación del paquete, con esta línea indico que la Clase LoginSceneController
-esta dentro del paquete com.gameapp.controller*/
+/** 
+ * Creación del paquete, con esta línea indico que la Clase LoginSceneController
+ * esta dentro del paquete com.gameapp.controller
+ */
 package com.gameapp.controller;
 
-// Importación de la clase ConstantsUtility del paquete com.gameapp.utility
+/**
+ * Importación de la clase ConstantsUtility del paquete com.gameapp.utility
+ */
 import com.gameapp.utility.ConstantsUtility;
 
-// Importación de la clase MessageUtility del paquete com.gameapp.utility
+/**
+ * Importación de la clase MessageUtility del paquete com.gameapp.utility
+ */
 import com.gameapp.utility.MessageUtility;
 
-// Importación de las librerias de javafx
+/**
+ * Importación de las librerias de javafx
+ */ 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -20,38 +28,47 @@ import javafx.scene.control.TextField;
  * class clase, 
  * LoginSceneController nombre que le estamos dando a la clase que contrendra 
  * la primera pantalla de nuestro juego donde se podra loguear el jugador
- *   
  */
 public class LoginSceneController {
-
-    @FXML /*@FXML esta anotación indica que el elemento que aparecerá a continuación en el código está
-    asociado a algún elemento del archivo FXML*/
+    /**
+     * @FXML esta anotación indica que el elemento que aparecerá a continuación en el código está
+     * asociado a algún elemento del archivo FXML
+     */
+    @FXML
     
-    private Button startGamebtn; //Creación del Botón para iniciar el juego
+    /**
+     * Creación del Botón para iniciar el juego
+     */
+    private Button startGamebtn;
 
-    @FXML /*@FXML esta anotación indica que el elemento que aparecerá a continuación en el código está
-    asociado a algún elemento del archivo FXML*/
+    @FXML 
     
-    private TextField userNameInput;//Creación del la caja de texto donde el jugador ingresará su nombre
+    /**
+     * Creación del la caja de texto donde el jugador ingresará su nombre
+     */
+    private TextField userNameInput;
 
-    @FXML /*@FXML esta anotación indica que el elemento que aparecerá a continuación en el código está
-    asociado a algún elemento del archivo FXML*/
+    @FXML 
     
-    /*Método cerrar en este se definen las acciones que se realizarán al presionar el 
-    botón cerrar que estará en la pantalla de inicio del juego*/ 
+    /**Método cerrar en este se definen las acciones que se realizarán al presionar el 
+     * botón cerrar que estará en la pantalla de inicio del juego
+     */ 
     private void close(ActionEvent event) {
         
-        /**MessageUtility.showMessage(Alert.AlertType.INFORMATION indica que se presentara un mensaje de tipo alerta
-        *ConstantsUtility.CLOSE_MESSAGE_TITLE  llamando el mensaje 
+        /**
+        * MessageUtility.showMessage(Alert.AlertType.INFORMATION indica que se presentara un mensaje de tipo alerta
+        * ConstantsUtility.CLOSE_MESSAGE_TITLE  llamando el mensaje 
         * "Se cerrará la ventana" que se presentará como titulo en el mensaje de Alerta este mensaje se encuentra definido
         * en el paquete  com.gameapp.utility en la clase ConstantsUtility.java
-        *ConstantsUtility.CLOSE_MESSAGE_CONTENT llamando el mensaje "Gracias por jugar hasta una proxima ocación" 
-        que se presentara en cuerpo del mensaje de alerta, este mensaje se encuentra definido en el paquete
+        * ConstantsUtility.CLOSE_MESSAGE_CONTENT llamando el mensaje "Gracias por jugar hasta una proxima ocación" 
+        * que se presentara en cuerpo del mensaje de alerta, este mensaje se encuentra definido en el paquete
         * com.gameapp.utility en la clase ConstantsUtility.java
         */
         MessageUtility.showMessage(Alert.AlertType.INFORMATION, ConstantsUtility.CLOSE_MESSAGE_TITLE,
                 ConstantsUtility.CLOSE_MESSAGE_CONTENT);
-        //Ejecuta el método salir del sistema
+        /**
+         * Ejecuta el método salir del sistema 
+         */
         System.exit(0);
     }
 
